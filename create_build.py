1b8c483e7232501a3aa5801a60a9954ce574236c
Joseph Eagar <joeedh@gmail.com>
2023-08-22 12:17:48 -0700

    Update package scripts

---
+++ create_build.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d96cb8e90c86e68f51141a4c76fe830cdbc67bc3c453592f4549b53a7e7eadc
+size 1269
--- make_package.py
+++ make_package.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c806425bc0745b9f457a6ba31524e87eac7f6694d05e5cfb14815bfb82694156
-size 3858
+oid sha256:4b4dd9511dd36d1897b205010f76f0292766ab7a3bbbe6c3d2f53f83cfb4f65f
+size 3930
